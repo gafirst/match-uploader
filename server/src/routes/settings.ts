a06b78eec78162603459cd5c81fa088cdfe42564
Evan Strat <evan10s@users.noreply.github.com>
2023-03-12 17:55:07 -0400

    Add settings service/APIs

---
+++ server/src/routes/settings.ts
@@ -0,0 +1,44 @@
+import {NextFunction, Router} from "express";
+import Paths from "@src/routes/constants/Paths";
+import {IReq, IRes} from "@src/routes/types/types";
+import {getSettings, setSetting} from "@src/services/SettingsService";
+import jetValidator from "jet-validator";
+import {body, check, matchedData, param, validationResult} from "express-validator";
+
+export const settingsRouter = Router();
+
+
+settingsRouter.get(
+	Paths.Settings.List,
+	listSettings,
+);
+
+async function listSettings(req: IReq, res: IRes) {
+	return res.json(await getSettings());
+}
+
+settingsRouter.post(
+	Paths.Settings.Update,
+	param("name", "Setting name to be updated is required and must be a string").isString(),
+	body("value", "New setting value is required and must be a string").isString().trim(),
+	updateSetting,
+)
+
+async function updateSetting(req: IReq<{ value: string }>, res: IRes) {
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400)
+			.json({
+				errors: errors.array(),
+			});
+	}
+
+	const {name, value} = matchedData(req);
+
+	await setSetting(name, value);
+
+	return res.json({
+		ok: true,
+	});
+}
