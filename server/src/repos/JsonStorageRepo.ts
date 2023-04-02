2c648bf40ae57d6cade39a3463a38895847ee467
Evan Strat <evan10s@users.noreply.github.com>
2023-04-01 20:21:42 -0400

    Remove unused default param value

--- server/src/repos/JsonStorageRepo.ts
+++ server/src/repos/JsonStorageRepo.ts
@@ -1,10 +1,10 @@
 import {Path, readFile, writeFile} from "jsonfile";
 
-export async function readSettingsJson<T>(file: Path = "./settings/settings.json"): Promise<T> {
+export async function readSettingsJson<T>(file: Path): Promise<T> {
 	// Paths are relative to the directory the server is running out of
 	return await readFile(file);
 }
 
-export async function writeSettingsJson<T>(newJson: T, file: Path = "./settings/settings.json") {
+export async function writeSettingsJson<T>(newJson: T, file: Path) {
 	return await writeFile(file, newJson);
 }
