8b51181bcabb6bee48554194f110f328cae6810d
Evan Strat <evan10s@users.noreply.github.com>
2023-10-09 01:34:46 -0400

    Formatting

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -46,17 +46,15 @@
       <VRow>
         <MatchVideosUploader />
       </VRow>
-
-      <!--      <h2>YouTube channel</h2>-->
-      <!--      <VRow align="center">-->
-      <!--        <VCol>-->
-      <!--          <VAvatar v-if="selectedYouTubeChannel && selectedYouTubeChannel.thumbnailUrl"-->
-      <!--                   :image="selectedYouTubeChannel.thumbnailUrl"-->
-      <!--          />-->
-
-      <!--          {{ selectedYouTubeChannel?.title ?? "No channel selected" }}-->
-      <!--        </VCol>-->
-      <!--      </VRow>-->
+      <VRow>
+        <VCol>
+          <h2 class="mb-2">Help</h2>
+          <VExpansionPanels>
+            <NameMatchVideoFilesHelp />
+            <MissingMatchVideosHelp />
+          </VExpansionPanels>
+        </VCol>
+      </VRow>
     </VCol>
   </VRow>
 </template>
@@ -66,34 +64,12 @@ import {ref} from "vue";
 import MatchSelector from "@/components/matches/MatchSelector.vue";
 import MatchVideosUploader from "@/components/matches/MatchVideosUploader.vue";
 import {useMatchStore} from "@/stores/match";
+import NameMatchVideoFilesHelp from "@/components/help/NameMatchVideoFilesHelp.vue";
+import MissingMatchVideosHelp from "@/components/help/MissingMatchVideosHelp.vue";
 
 const error = ref("");
 
 const matchStore = useMatchStore();
-
-// const { data: youTubeStatus, error: youTubeStatusError } = useSWRV("/api/v1/youtube/status");
-
-// watch(youTubeStatusError, (err) => {
-//   console.error("YouTube status error:", err.message);
-//   error.value = `Error obtaining authenticated YouTube channel info: ${err.message}`;
-// });
-//
-// function isYouTubeStatus(obj: object): obj is IYouTubeStatus {
-//   return !!(obj as IYouTubeStatus).channels;
-// }
-
-// const selectedYouTubeChannel = computed(() => {
-//   if (!youTubeStatus.value) {
-//     console.log("no status");
-//     return null;
-//   }
-//
-//   if (!isYouTubeStatus(youTubeStatus.value)) {
-//     return null;
-//   }
-//
-//   return youTubeStatus.value.channels.find((channel) => channel.id === matchStore.youTubeChannelId);
-// });
 
 </script>
 
