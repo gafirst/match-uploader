a0b41e7e6e0f200212858cffee9da115789956f5
Evan Strat <evan10s@users.noreply.github.com>
2023-10-14 19:27:48 -0400

    Frontend updates for description generation

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -17,15 +17,7 @@
       >
         Video metadata
       </h2>
-      <VTextarea v-model="matchStore.description"
-                 label="Description"
-                 :disabled="matchStore.uploadInProgress"
-                 messages="Raw YouTube video description"
-                 class="mb-4"
-      />
-
-      <!--      <h3>YouTube</h3>-->
-      <!--      <YouTubeChannelSelector class="mt-2 mb-4" />-->
+      <MatchDescription />
     </VCol>
     <VCol cols="12" md="7">
       <h2 class="mb-2">Videos</h2>
@@ -46,17 +38,6 @@
       <VRow>
         <MatchVideosUploader />
       </VRow>
-
-      <!--      <h2>YouTube channel</h2>-->
-      <!--      <VRow align="center">-->
-      <!--        <VCol>-->
-      <!--          <VAvatar v-if="selectedYouTubeChannel && selectedYouTubeChannel.thumbnailUrl"-->
-      <!--                   :image="selectedYouTubeChannel.thumbnailUrl"-->
-      <!--          />-->
-
-      <!--          {{ selectedYouTubeChannel?.title ?? "No channel selected" }}-->
-      <!--        </VCol>-->
-      <!--      </VRow>-->
     </VCol>
   </VRow>
 </template>
@@ -66,35 +47,11 @@ import {ref} from "vue";
 import MatchSelector from "@/components/matches/MatchSelector.vue";
 import MatchVideosUploader from "@/components/matches/MatchVideosUploader.vue";
 import {useMatchStore} from "@/stores/match";
+import MatchDescription from "@/components/matches/MatchDescription.vue";
 
 const error = ref("");
 
 const matchStore = useMatchStore();
-
-// const { data: youTubeStatus, error: youTubeStatusError } = useSWRV("/api/v1/youtube/status");
-
-// watch(youTubeStatusError, (err) => {
-//   console.error("YouTube status error:", err.message);
-//   error.value = `Error obtaining authenticated YouTube channel info: ${err.message}`;
-// });
-//
-// function isYouTubeStatus(obj: object): obj is IYouTubeStatus {
-//   return !!(obj as IYouTubeStatus).channels;
-// }
-
-// const selectedYouTubeChannel = computed(() => {
-//   if (!youTubeStatus.value) {
-//     console.log("no status");
-//     return null;
-//   }
-//
-//   if (!isYouTubeStatus(youTubeStatus.value)) {
-//     return null;
-//   }
-//
-//   return youTubeStatus.value.channels.find((channel) => channel.id === matchStore.youTubeChannelId);
-// });
-
 </script>
 
 <style>
