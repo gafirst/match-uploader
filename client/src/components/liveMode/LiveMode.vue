0314d8f91ff5c1f9c434b62523ade4f9deb2f6ee
Evan Strat <evan10s@users.noreply.github.com>
2024-05-10 00:23:00 -0400

    Lots of continued live mode progress

--- client/src/components/liveMode/LiveMode.vue
+++ client/src/components/liveMode/LiveMode.vue
@@ -8,23 +8,28 @@
       <VAlert color="purple"
               variant="tonal"
               class="mb-2"
+              density="compact"
+              icon="mdi-bug-outline"
       >
         Report bugs and send feedback
         <a target="_blank" href="https://github.com/gafirst/match-uploader/issues/new/choose">on GitHub</a>.
       </VAlert>
-      <p class="mb-2">
-        Live Mode watches for video files and queues uploads once all of a match's
-        <strong>expected</strong> video files are found.
-      </p>
-      <p class="mb-2">
-        <strong>What makes a video file expected?</strong> If you have a playlist mapping for a label, then Live Mode
-        will expect a video to exist for it.
-      </p>
+      <template v-if="!liveMode.isActive">
+        <p class="mb-2">
+          Live Mode watches for video files and queues uploads once all of a match's
+          <strong>expected</strong> video files are found.
+        </p>
+        <p class="mb-2">
+          <strong>What makes a video file expected?</strong> If you have a playlist mapping for a label, then Live Mode
+          will expect a video to exist for it.
+        </p>
 
+        <VDivider class="mb-2 border-opacity-25" thickness="2" />
+      </template>
       <template v-if="!liveMode.isAllowed">
         <p>Some requirements to enable Live Mode are not met:</p>
         <VList>
-          <TaskListItem v-for="[requirement, requirementMet] in Object.entries(liveMode.requirements)"
+          <TaskListItem v-for="[requirement, requirementMet] in Object.entries(liveMode.requirements).filter(([_, met]) => !met)"
                         :key="requirement"
                         :complete="requirementMet"
                         density="compact"
@@ -33,24 +38,108 @@
           </TaskListItem>
         </VList>
       </template>
-      <VAlert v-else
+      <VAlert v-else-if="!liveMode.isActive"
               variant="outlined"
-              color="info"
+              color="success"
+              density="compact"
               icon="mdi-information-outline"
-              class="mb-2"
+              class="mb-4"
       >
-        Ready to enable!
+        Live Mode is ready!
       </VAlert>
+      <template v-else>
+        <h2 class="mt-4 mb-2">
+          Live Mode is active <VChip v-if="liveMode.isFastActive">Fast</VChip><VChip v-if="liveMode.isSlowActive">Slow</VChip>
+        </h2>
+        <p class="mb-3">
+          Keep this tab open—Live Mode runs in your browser.
+        </p>
 
-      <!--      <VBtn @click="liveMode.resume">Start Live Mode</VBtn>-->
-      <!--      <VBtn @click="liveMode.pause">Stop Live Mode</VBtn>-->
-      <p>Live Mode state: {{ liveMode.isActive }}</p>
-      <p>Live Mode status: {{ liveMode.state }}</p>
+        <VAlert v-if="liveMode.error || liveMode.state === LiveModeStatus.ERROR"
+                variant="outlined"
+                color="error"
+                density="compact"
+                icon="mdi-alert-circle"
+                class="mb-4"
+        >
+          <VAlertTitle class="mb-2">Live Mode is paused due to an error</VAlertTitle>
+          <p class="mb-2">{{ liveMode.error ?? "An unknown error occurred" }}</p>
+
+          <p class="mb-2">Once you've resolved the error, press <strong>Clear error</strong> below to try again.</p>
+          <VBtn class="mb-2" @click="liveMode.clearErrorAndTick">Clear error</VBtn>
+        </VAlert>
+
+        <VAlert v-else-if="liveMode.missingPlaylistLabels.length > 0"
+                variant="outlined"
+                color="warning"
+                density="compact"
+                icon="mdi-alert-circle"
+                class="mb-4"
+        >
+          Missing required video{{ liveMode.missingPlaylistLabels.length > 1 ? "s" : "" }}:
+          {{ liveMode.missingPlaylistLabels.sort().join(", ") }}
+        </VAlert>
+
+        <VAlert v-if="liveMode.state === LiveModeStatus.WAITING"
+                variant="outlined"
+                color="purple"
+                density="compact"
+                icon="mdi-sleep"
+                class="mb-4"
+        >
+          <template v-if="liveMode.isFastActive">
+            Sleeping, next refresh in a few seconds
+          </template>
+          <template v-else>
+            Sleeping, next refresh at {{ dayjs(liveMode.estimatedNextTick).format("h:mm A") }}
+          </template>
+        </VAlert>
+
+        <VAlert v-if="liveMode.state === LiveModeStatus.ADVANCE_MATCH"
+                variant="outlined"
+                color="info"
+                density="compact"
+                icon="mdi-loading mdi-spin"
+                class="mb-4"
+        >
+          Advancing match...
+        </VAlert>
+        <VAlert v-if="liveMode.state === LiveModeStatus.FETCH_VIDEOS"
+                variant="outlined"
+                color="info"
+                density="compact"
+                icon="mdi-loading mdi-spin"
+                class="mb-4"
+        >
+          Fetching videos...
+        </VAlert>
+        <VAlert v-if="liveMode.state === LiveModeStatus.QUEUE_UPLOADS"
+                variant="outlined"
+                color="info"
+                density="compact"
+                icon="mdi-loading mdi-spin"
+                class="mb-4"
+        >
+          Queueing videos for upload...
+        </VAlert>
+
+        <VAlert v-if="liveMode.state === LiveModeStatus.STOPPED"
+                variant="outlined"
+                color="gray"
+                density="compact"
+                icon="mdi-pause"
+                class="mb-4"
+        >
+          Paused, press Activate below to resume
+        </VAlert>
+      </template>
+
       <VBtn v-if="liveMode.isAllowed && !liveMode.isActive"
-            class="mr-2"
+            class="mr-2 mb-0"
+            color="purple"
             @click="liveMode.activate"
       >
-        Activate
+        Start Live Mode
       </VBtn>
       <VBtn v-if="liveMode.isActive"
             class="mr-2"
@@ -58,17 +147,22 @@
       >
         Deactivate
       </VBtn>
-      <VBtn v-if="liveMode.isActive" @click="liveMode.triggerImmediateTick">Check now</VBtn>
+      <VBtn v-if="liveMode.isActive"
+            :disabled="!!liveMode.error"
+            @click="liveMode.triggerImmediateTick"
+      >
+        Check now
+      </VBtn>
     </VCardText>
   </VCard>
 </template>
 <script lang="ts" setup>
 import { useLiveModeStore } from "@/stores/liveMode";
-import {
-  liveModeRequirementToErrorString,
-  liveModeRequirementToUiString,
-} from "../../types/liveMode/LiveModeRequirements";
+import { liveModeRequirementToUiString } from "@/types/liveMode/LiveModeRequirements";
 import TaskListItem from "@/components/util/TaskListItem.vue";
+import dayjs from "dayjs";
+import { LiveModeStatus, liveModeStatusToUiString } from "@/types/liveMode/LiveModeStatus";
+import { computed } from "vue";
 
 const liveMode = useLiveModeStore();
 </script>
--- client/src/stores/liveMode.ts
+++ client/src/stores/liveMode.ts
@@ -7,14 +7,43 @@ import { useMatchStore } from "@/stores/match";
 import { useSettingsStore } from "@/stores/settings";
 import { PLAYOFF_DOUBLE_ELIM } from "@/types/MatchType";
 import { LiveModeRequirements, LiveModeUnsatisfiedRequirements } from "@/types/liveMode/LiveModeRequirements";
+import LiveMode from "@/components/liveMode/LiveMode.vue";
 
 export const useLiveModeStore = defineStore("liveMode", () => {
   const lastTick = ref<Date | null>(null);
+
+  const slowTickIntervalMilliseconds = 60 * 5 * 1000; // 5 minutes in milliseconds
+  const fastTickIntervalMilliseconds = 10 * 1000; // 10 seconds in milliseconds
+
+  const tickIntervalMilliseconds = computed(() => {
+    if (isFastActive.value) {
+      return fastTickIntervalMilliseconds;
+    }
+
+    if (isSlowActive.value) {
+      return slowTickIntervalMilliseconds;
+    }
+
+    return null;
+  });
+
   const state = ref<LiveModeStatus>(LiveModeStatus.STOPPED);
+  const error = ref<string | null>(null);
 
   const settingsStore = useSettingsStore();
   const playlistStore = usePlaylistsStore();
   const matchStore = useMatchStore();
+
+  const lastAction = ref<LiveModeStatus | null>(null);
+  const missingPlaylistLabels = ref<string[]>([]);
+
+  const estimatedNextTick = computed(() => {
+    if (!lastTick.value || !tickIntervalMilliseconds.value) {
+      return null;
+    }
+
+    return new Date(lastTick.value.getTime() + tickIntervalMilliseconds.value);
+  });
 
   const requirements = computed<LiveModeRequirements>(() => {
     return {
@@ -24,16 +53,29 @@ export const useLiveModeStore = defineStore("liveMode", () => {
     };
   });
 
+  function setError(errorString: string) {
+    state.value = LiveModeStatus.ERROR;
+    error.value = errorString;
+    setFastTicks(false);
+  }
+
   const isAllowed = computed(() => {
     return Object.values(requirements.value).every(requirementMet => requirementMet);
   });
-  async function areAllVideosPresent() {
+
+  async function areAllRequiredVideosPresent() {
+    missingPlaylistLabels.value = [];
     await playlistStore.getPlaylists();
 
-    if (!playlistStore.playlists) {
-      return false; // FIXME: this should trigger an error
+    console.log("playlistStore.playlists", playlistStore.playlists);
+
+    if (playlistStore.error || !playlistStore.playlists) {
+      setError("Cannot determine if all required videos are present due to an error loading playlist " +
+        `data: ${playlistStore.error}`);
+      return false;
     }
 
+    // FIXME: does not return all missing labels because of `every`
     return playlistStore.playlists.every((playlist) => {
       const result = matchStore.matchVideos.some((video) => {
           console.log("video.videoLabel?.toLowerCase() ?? \"unlabeled\"", video.videoLabel?.toLowerCase() ?? "unlabeled");
@@ -44,6 +86,10 @@ export const useLiveModeStore = defineStore("liveMode", () => {
 
       console.log("result for playlist", playlist.label, result);
 
+      if (!result) {
+        missingPlaylistLabels.value.push(playlist.label);
+      }
+
       return result;
     });
   }
@@ -52,46 +98,95 @@ export const useLiveModeStore = defineStore("liveMode", () => {
     lastTick.value = new Date();
 
     console.log(new Date().toISOString(), "Live mode tick:", state.value);
-    if (state.value === LiveModeStatus.WAITING) {
+    if (state.value !== LiveModeStatus.WAITING) {
+      console.log(`Live mode tick: state is ${state.value}, stopping tick`);
+      return;
+    }
+
+    let attempts = 0;
+    const maxAttempts = 2;
+    let shouldCheckAgain = true;
+
+    // FIXME: what happens if the description isn't available?
+    while (attempts < maxAttempts && shouldCheckAgain) {
+      attempts++;
+
       state.value = LiveModeStatus.FETCH_VIDEOS;
       await matchStore.getMatchVideos();
 
-      // fixme: all this nesting is gross
-      if (await areAllVideosPresent()) {
+      if (await areAllRequiredVideosPresent()) {
+        setFastTicks(true);
         state.value = LiveModeStatus.QUEUE_UPLOADS;
-        // await matchStore.queueMatchVideos();
-        if (!matchStore.allowMatchUpload) {
-          console.error("Cannot trigger upload right now, check matchStore"); // fixme
-        } else {
-          await matchStore.uploadVideos();
-          // fixme: check if it succeeded?
 
+        if (matchStore.allMatchVideosUploaded) {
           state.value = LiveModeStatus.ADVANCE_MATCH;
           await matchStore.advanceMatch();
-          state.value = LiveModeStatus.WAITING;
+        } else if (!matchStore.allowMatchUpload) {
+          shouldCheckAgain = false;
+          setError("Videos cannot be queued for upload right now. Check the upload form for errors.");
+          console.error("Cannot trigger upload right now, check matchStore");
+        } else {
+          await matchStore.uploadVideos(); // FIXME: check if all upload job creations succeeded?
+          if (matchStore.allMatchVideosQueued) {
+            state.value = LiveModeStatus.ADVANCE_MATCH;
+            await matchStore.advanceMatch();
+          } else {
+            shouldCheckAgain = false;
+            setError("All videos were not queued for upload; please check for errors in the upload form");
+          }
         }
       } else {
-        state.value = LiveModeStatus.WAITING;
+        if (error.value) {
+          state.value = LiveModeStatus.ERROR;
+        }
+        // If videos aren't present, drop back to slow mode
+        setFastTicks(false);
+        shouldCheckAgain = false;
       }
+    }
+
+    state.value = LiveModeStatus.WAITING;
+  }
+
+  const {
+    pause: slowPause,
+    resume: slowResume,
+    isActive: isSlowActive,
+  } = useIntervalFn(liveModeTick, slowTickIntervalMilliseconds, { immediate: false });
+
+  const {
+    pause: fastPause,
+    resume: fastResume,
+    isActive: isFastActive,
+  } = useIntervalFn(liveModeTick, fastTickIntervalMilliseconds, { immediate: false });
+
+  function setFastTicks(enableFastTicks: boolean) {
+    console.log(`setFastTicks: ${enableFastTicks}`);
+    if (enableFastTicks) {
+      slowPause();
+      fastResume(); // FIXME: is it problematic to call this twice?
+    } else {
+      fastPause();
+      slowResume();
     }
   }
 
-  const { pause, resume, isActive } = useIntervalFn(liveModeTick, 60 * 5 * 1000, {
-    immediate: false,
-  });
-
   async function activate() {
-    if (!matchStore.selectedMatchKey) {
-      return; // FIXME: throw error
+    if (!matchStore.selectedMatchKey) { // FIXME: replace with requirements check
+      console.error("Cannot activate live mode because a match is not selected");
+      state.value = LiveModeStatus.ERROR;
+      return;
     }
 
-    resume();
     state.value = LiveModeStatus.WAITING;
+    error.value = null;
+    fastResume();
     await triggerImmediateTick();
   }
 
   function deactivate() {
-    pause();
+    fastPause();
+    slowPause();
     state.value = LiveModeStatus.STOPPED;
   }
 
@@ -99,12 +194,28 @@ export const useLiveModeStore = defineStore("liveMode", () => {
     await liveModeTick();
   }
 
+  async function clearErrorAndTick() {
+    error.value = null;
+    state.value = LiveModeStatus.WAITING;
+    await triggerImmediateTick();
+  }
+
+  const isActive = computed(() => {
+    return isSlowActive.value || isFastActive.value;
+  });
+
   return {
     activate,
+    clearErrorAndTick,
     deactivate,
+    error,
+    estimatedNextTick,
     isActive,
+    isFastActive,
+    isSlowActive,
     isAllowed,
     lastTick,
+    missingPlaylistLabels,
     requirements,
     state,
     triggerImmediateTick,
