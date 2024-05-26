fa879e5ed944d1c84dd4ec30a3298beadebc8238
Evan Strat <evan10s@users.noreply.github.com>
2024-05-26 18:34:06 -0400

    use global max-len rule

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -29,10 +29,7 @@ module.exports = {
         "void": "always",
       },
     }],
-    "max-len": "off",
-    "vue/max-len": ["error", {
-      "code": 120,
-    }],
+    "vue/max-len": "off",
     "vue/max-attributes-per-line": ["error", {
       singleline: 2,
     }],
